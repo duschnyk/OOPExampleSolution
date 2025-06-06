6fcde3963f7317335293efa1331ca27c90bb22fc
giganig <matveykaschuk@gmail.com>
2025-06-06 18:47:05 +0300

    Update Student constructors

--- MainProject/main.cpp
+++ MainProject/main.cpp
@@ -1,19 +1,17 @@
-#include "main.h"
-#include "plane.h"
+#include "Student.h"
 
-void print(Plane plane);
 
 int main() {
 	Student st1;
-	Student st2("Alex", "Chichikov");
+	/*Student st2("Alex", "Chichikov");
 	Student st3("Ivan", "Ivanov", 15);
 	Student st4("Olya", "Sidorova", 14, 8, 'f', true, 9);
-	Student st5(st4);
+	Student st5(st4);*/
 
 	cout << st1.toString() << endl;
-	cout << st2.toString() << endl;
+	/*cout << st2.toString() << endl;
 	cout << st3.toString() << endl;
-	cout << st4.toString() << endl;
+	cout << st4.toString() << endl;*/
 
 	return 0;
 }
