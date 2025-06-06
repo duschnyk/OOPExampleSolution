4560dbc08c04a37d9a705e57156ed67f7c6ec277
giganig <matveykaschuk@gmail.com>
2025-06-06 19:44:36 +0300

    Update

--- MainProject/main.cpp
+++ MainProject/main.cpp
@@ -1,17 +1,24 @@
-#include "Student.h"
+#include "initializer.h"
 
 
 int main() {
-	Student st1;
-	/*Student st2("Alex", "Chichikov");
-	Student st3("Ivan", "Ivanov", 15);
-	Student st4("Olya", "Sidorova", 14, 8, 'f', true, 9);
-	Student st5(st4);*/
+	int size;
 
-	cout << st1.toString() << endl;
-	/*cout << st2.toString() << endl;
-	cout << st3.toString() << endl;
-	cout << st4.toString() << endl;*/
+	cout << "Input size";
+	cin >> size;
+
+	Student* students = new Student[size];
+
+	Initializer initializer;
+
+	initializer.init(students, size);
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << students[i].toString() << endl;
+	}
+
+	delete[] students;
 
 	return 0;
 }
